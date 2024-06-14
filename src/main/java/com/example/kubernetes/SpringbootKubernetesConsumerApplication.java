3b4e74be5331c92ca6fbc3fc9101aee980b47377
4631 <alessio.zubiani@popso.it>
2024-06-14 15:12:27 +0200

    Creazione progetto Kubernetes Consumer

---
+++ src/main/java/com/example/kubernetes/SpringbootKubernetesConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKubernetesConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKubernetesConsumerApplication.class, args);
+	}
+
+}
